044eb608613ac1c58a32fe1d79e08869c284a6b7
dtrowbri <dtrowbridg@my.gcu.edu>
2022-02-13 01:29:34 -0700

    Added very basic user controller and web page to test the add user dao method

---
+++ CST326/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CST326.Models;
+using CST326.DAO;
+
+namespace CST326.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserModel user)
+        {
+            UserDAO dao = new UserDAO();
+
+            var results = dao.AddUser(user);
+
+            return Content(results.ToString());
+        } 
+    }
+}
