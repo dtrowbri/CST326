1b4bfcd1821450935753eb0bb98c9e10730cb56a
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2022-02-22 18:20:36 -0700

    Add Employee Model/DAO
    Add employee model and DAO
    Add employee Login page and functionality

--- CST326/Controllers/EmployeeController.cs
+++ CST326/Controllers/EmployeeController.cs
@@ -13,5 +13,10 @@ namespace CST326.Controllers
         {
             return View();
         }
+
+        public ActionResult EmployeeLogin()
+        {
+            return View();
+        }
     }
 }
--- CST326/Models/EmployeeModel.cs
+++ CST326/Models/EmployeeModel.cs
@@ -12,6 +12,7 @@ namespace CST326.Models
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public int Admin { get; set; }
 
         public EmployeeModel(int employeeId, string firstName, string lastName, string phoneNumber, string email, int admin)
